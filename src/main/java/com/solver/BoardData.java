6e4f49ae77082a14cbd18bb5a0c2b9083552a636
mental3d <mental3d@gmail.com>
2014-02-24 17:46:50 +0200

    add function copy in BoardData

--- src/main/java/com/solver/BoardData.java
+++ src/main/java/com/solver/BoardData.java
@@ -9,12 +9,52 @@ import com.utils.Board;
  * Time: 16:01
  */
 public class BoardData {
-    private ArrayList<ArrayList<int>> board = new ArrayList<ArrayList<int>>();
+    private ArrayList<ArrayList<Integer>> cellBoard = new ArrayList<ArrayList<Integer>>();
+    private int height, width;
+    private int id;
 
-    public BoardData(int index, Board board)
+    public BoardData(int id, Board board)
+    {
+        this.id = id;
+        initCellBoard(board.boardSize(), board.boardSize()); //fix внтури boardSize() тяжелая функция можно выполнить один раз в BoardRaw, так как размер доски const
+    }
+
+    // Copy constructor
+    public BoardData(BoardData bd)
     {
 
     }
 
-    private void
+
+    private void initCellBoard(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        for(int i = 0; i < width; i++)
+        {
+            cellBoard.add(new ArrayList<Integer>());
+        }
+    }
+
+
+    private void convert(Board board)
+    {
+
+    }
+
+    public int getCell(int x, int y)
+    {
+        return  cellBoard.get(x).get(y);
+    }
+
+    public  ArrayList<ArrayList<Integer>> getCloneCellBoard()
+    {
+        ArrayList<ArrayList<Integer>> array = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i < width; i++)
+        {
+            array.add(new ArrayList<Integer>(cellBoard.get(i)));
+        }
+        return  array;
+    }
+
 }
