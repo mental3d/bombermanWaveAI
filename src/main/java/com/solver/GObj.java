4dff3c0b13956ac0c5b5d962613012706d58daa1
mental3d <mental3d@gmail.com>
2014-02-25 17:48:52 +0200

    сделал нормальный enum для GameObject

---
+++ src/main/java/com/solver/GObj.java
@@ -0,0 +1,18 @@
+package com.solver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 21.02.14
+ * Time: 16:16
+ */
+public enum GObj {
+        FREE,
+        BARRIERS,
+        WALLS,
+        BOMBS,
+        BOMBERMAN,
+        CHOPPERS,
+        BLASTS
+
+}
